41e2bec6fedc6d223bed38bc700bad73d20cbe82
KampusBiner <60420873+kampusbiner@users.noreply.github.com>
2020-01-29 15:06:03 +0700

    Add files via upload

---
+++ Uninstall.cmd
@@ -0,0 +1,11 @@
+@echo off
+cd /d "%~dp0"
+echo Uninstalling ...
+CLS
+install_wim_tweak.exe /o /l
+install_wim_tweak.exe /o /c "Windows-Defender" /r
+
+
+install_wim_tweak.exe /h /o /l
+echo It should be uninstalled. Please reboot Windows 10.
+pause
